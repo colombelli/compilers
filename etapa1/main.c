88231070e6b89f25b0de0760ac0a017dfc858abc
colombelli <xcolombelli@gmail.com>
2020-08-25 17:04:39 -0300

    changed logic to force input texts being read from the inputs folder

--- etapa1/main.c
+++ etapa1/main.c
@@ -1,10 +1,16 @@
+#include <string.h>
+
 int main (int argc, char **argv) {
   
+  char fileDest[80] = "inputs/";
+
   if (argc < 2){
     fprintf(stderr, "Call: etapa1 fileName\n");
     exit(1);
   }
-  yyin = fopen(argv[1], "r");
+  
+  strcat(fileDest, argv[1]);
+  yyin = fopen(fileDest, "r");
   if (yyin == 0){
     fprintf(stderr, "Cannot open file %s\n", argv[1]);
     exit(2);
